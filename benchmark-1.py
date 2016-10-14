f2d84cf3d95ac6e856bdbe8fa950b92fea4e814c
Andrei Regiani <andrei.cpp@gmail.com>
2016-10-14 04:45:54 +0300

    Support code improvements

--- benchmark-1.py
+++ benchmark-1.py
@@ -2,26 +2,25 @@
 
 import sys
 import time
-# pode adicionar mais, porem para esse case-study apenas biblioteca padrão
-# favor nao injetar time.sleep() no codigo dos outros
-# depois podemos fazer em NumPy e outros para comparar
+# you may import others, but for this case-study use only standard library
+# please do not add time.sleep() into someone else's algorithm :^)
+# later compare with NumPy to see how optmized they actually are?
 
 """
 Criteria:
 * fastest runtime processing
-* handles big data
+* handles big data without crashing
 
-You are given an array (which will have a length of at least 3, 
-but could be very large) containing int_list. 
-The array is either entirely comprised of odd int_list or 
-entirely comprised of even int_list except for a single integer N. 
+You are given an array (which will have a length of at least 3,
+but could be very large) containing int_list.
+The array is either entirely comprised of odd int_list or
+entirely comprised of even int_list except for a single integer N.
 Write a method that takes the array as an argument and returns N.
 
 For example:
 
 [2, 4, 0, 100, 4, 11, 2602, 36]
 Should return: 11
-
 
 [160, 3, 1719, 19, 11, 13, -21]
 Should return: 160
@@ -31,10 +30,12 @@ def benchmark(function, big_list, expected_output):
         start = time.time()
         if function(big_list) == expected_output:
                 elapsed = time.time() - start
-                print("{:<20} {}".format(function.__name__, elapsed))
-        
-        
-"""Algorithms"""
+                print("{:<20} {:.2f}".format(function.__name__, elapsed))
+
+
+"""
+Algorithms
+"""
 
 def luiz_1(int_list):
     if int_list[0] % 2 == int_list[1] % 2:
@@ -43,29 +44,35 @@ def luiz_1(int_list):
         return int_list[1]
     else:
         return int_list[0]
-        
-        
+
+
 def andrei_1(int_list):
     odds = [x for x in int_list if x % 2]
     evens = set(odds) ^ set(int_list)
     if len(evens) > len(odds): return odds.pop()
     return evens.pop()
 
+"""
+Main
+"""
 
-
-if len(sys.argv) != 2:
-        print("Usage: sample_size\nExample: 50000000")
-        sys.exit()
+# int_list size
+sample_size = 30000000
+#sample_size = 150000000  # BIG DATA - some algorithms may crash your computer
+try:
+    sample_size = int(sys.argv[1])  # or pass one by command-line argument
+except IndexError:
+    pass
 
 # generate list of integers (all odds, but one)
-sample_size = int(sys.argv[1])
 EVEN = 2
 random_odds = [i for i in range(sample_size) if i % 2]
 # insert one even number in the middle of the list
-random_odds[int(sample_size / 2 / 2)] = EVEN  # outsider!
+random_odds[int(sample_size / 2 / 2)] = EVEN  # outsider
 
-
-"""Run algorithms"""
+"""
+Run algorithms
+"""
 
 benchmark(luiz_1, random_odds, EVEN)
 benchmark(andrei_1, random_odds, EVEN)
